b6aafb31601162455232170451003e3161dfd639
Dark Angel <jonathanyombo@gmail.com>
2024-06-10 22:19:42 +0200

    [DEBUG] Member to User

--- database/migrations/2024_06_10_204259_create_cards_table.php
+++ database/migrations/2024_06_10_204259_create_cards_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Member;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Member::class);
+            $table->foreignIdFor(User::class);
             $table->string('picture_url')->nullable();
             $table->string('url')->nullable();
             $table->enum('state', ['initialized', 'verified', 'accepted', 'printed'])->default('initialized');
