7af400fc31e379bc6d57177076d4f5bc3adc3a0e
Dark Angel <jonathanyombo@gmail.com>
2024-06-11 01:37:40 +0200

    User resoruce (filament)

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -4,10 +4,16 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
 use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
+use Ysfkaya\FilamentPhoneInput\PhoneInputNumberType;
 
 class UserResource extends Resource
 {
@@ -19,7 +25,72 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->label('Nom')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('prenom')
+                    ->label('Prenom')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('postnom')
+                    ->maxLength(50)
+                    ->default(null),
+                PhoneInput::make('phone')
+                    ->label('Téléphone')
+                    ->defaultCountry('CD')
+                    ->displayNumberFormat(PhoneInputNumberType::INTERNATIONAL)
+                    ->required(),
+                Forms\Components\DatePicker::make('date_inscription')
+                    ->label("Date d'inscription")
+                    ->date()
+                    ->default(now()),
+                Forms\Components\TextInput::make('qr_code')
+                    ->hidden()
+                    ->maxLength(255)
+                    ->default(null),
+                Forms\Components\TextInput::make('email')
+                    ->afterStateHydrated(function ($state, $set) {
+                        if (str_ends_with($state, '@accd-rdc.org')) {
+                            $username = substr($state, 0, -13);
+                            $set('email', $username);
+                        }
+                    })
+                    ->dehydrateStateUsing(function ($state) {
+                        // Add the domain part before saving
+                        return $state . '@accd-rdc.org';
+                    })
+                    ->suffix('@accd-rdc.org')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('password')
+                    ->label('Mot de passe')
+                    ->password()
+                    ->required()
+                    ->maxLength(255)
+                    ->hiddenOn('edit'),
+                Forms\Components\Select::make('agency_id')
+                    ->relationship('agency', 'nom')
+                    ->label('Agence')
+                    ->default(null),
+                Forms\Components\FileUpload::make('avatar')
+                    ->avatar()
+                    ->image()
+                    ->imageEditor()
+                    ->imageEditorAspectRatios([
+                        '1:1',
+                    ])
+                    ->imageResizeMode('cover')
+                    ->imageCropAspectRatio('1:1')
+                    ->disk('public')
+                    ->directory('images')
+                    ->visibility('private')
+                    ->moveFiles(),
+                Forms\Components\Toggle::make('is_active')
+                    ->required(),
+                Select::make('roles')
+                    ->multiple()
+                    ->relationship('roles', 'name')
             ]);
     }
 
@@ -27,10 +98,46 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('prenom')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nom')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('postnom')
+                    ->searchable(),
+                Tables\Columns\ImageColumn::make('avatar')
+                    ->circular()
+                    ->defaultImageUrl(asset('/images/placeholder.jpg')),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Téléphone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('date_inscription')
+                    ->label('Affiliation')
+                    ->date('j M Y')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('agency.nom')
+                    ->label('Agence')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
-                //
+                Tables\Filters\TrashedFilter::make(),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
@@ -38,6 +145,8 @@ class UserResource extends Resource
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
                 ]),
             ]);
     }
@@ -56,5 +165,13 @@ class UserResource extends Resource
             'create' => Pages\CreateUser::route('/create'),
             'edit' => Pages\EditUser::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
     }
 }
