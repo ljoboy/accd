e65a18e6a88a6c70f645218af174ba56a8017f48
Dark Angel <jonathanyombo@gmail.com>
2024-06-11 01:36:06 +0200

    Agency model

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -2,10 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Agency extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+
+    protected $fillable = [
+        'nom',
+        'adresse',
+        'phone',
+        'ville',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected function nom(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => strtoupper($value),
+        );
+    }
 }
