a7acb29a91501cb750f54970d6a47d7fa23c98b8
Dark Angel <jonathanyombo@gmail.com>
2024-06-11 01:35:57 +0200

    USer model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Althinect\FilamentSpatieRolesPermissions\Concerns\HasSuperAdmin;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Propaganistas\LaravelPhone\Casts\PhoneNumberCast;
 
 /**
  * @property mixed $email
@@ -18,6 +20,8 @@ use Propaganistas\LaravelPhone\Casts\PhoneNumberCast;
 class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
+    use SoftDeletes;
+    use HasSuperAdmin;
 
     /**
      * The attributes that are mass assignable.
@@ -25,14 +29,19 @@ class User extends Authenticatable implements FilamentUser
      * @var array<int, string>
      */
     protected $fillable = [
-        'nom',
+        'name',
         'prenom',
         'postnom',
         'phone',
         'date_inscription',
         'email',
         'password',
-        'agency_id'
+        'agency_id',
+        'avatar',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'deleted_at',
     ];
 
     /**
@@ -55,8 +64,9 @@ class User extends Authenticatable implements FilamentUser
         return [
             'email_verified_at' => 'datetime:d/m/Y H:i',
             'date_inscription' => 'date:d-m-Y',
-            'phone' => PhoneNumberCast::class,
+//            'phone' => PhoneNumberCast::class,
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 
@@ -72,5 +82,25 @@ class User extends Authenticatable implements FilamentUser
     public function canAccessPanel(Panel $panel): bool
     {
         return str_ends_with($this->email, '@accd-rdc.org') && $this->is_active;
+    }
+
+    protected function name(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => strtoupper($value),
+        );
+    }
+
+    protected function prenom(): Attribute
+    {
+        return new Attribute(
+            get: fn($value) => ucfirst($value),
+        );
+    }
+    protected function postnom(): Attribute
+    {
+        return new Attribute(
+            get: fn ($value) => strtoupper($value),
+        );
     }
 }
